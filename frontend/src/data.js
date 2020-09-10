6c26cf5c88d63ae73bc0e626713f1ff54719dedc
peter <64981120+petermanchester1987@users.noreply.github.com>
2020-09-10 15:29:18 +0100

    built basic html and css elements

---
+++ frontend/src/data.js
@@ -0,0 +1,31 @@
+export default {
+  products: [
+    {
+      name: "Small Bouquet",
+      category: "Wedding",
+      image: "./images/d1.jpg",
+      price: 25,
+      brand: "Roses",
+      rating: 4.5,
+      numReview: 10,
+    },
+    {
+      name: "Medium Bouquet",
+      category: "Anniversary",
+      image: "./images/d2.jpg",
+      price: 40,
+      brand: "Roses",
+      rating: 5,
+      numReview: 20,
+    },
+    {
+      name: "Large Bouquet",
+      category: "Wedding",
+      image: "./images/d3.jpg",
+      price: 60,
+      brand: "Wedding",
+      rating: 4.8,
+      numReview: 5,
+    },
+  ],
+};
